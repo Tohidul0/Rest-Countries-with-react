7b3101b087cd12f6932c700dffbb021f0e74f94f
Tohidul0 <tohidulalam967418@gmail.com>
2023-12-15 23:49:38 +0600

    api use with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Boost></Boost>
+
+      <h1>All Countries Details</h1>
+      <Countries></Countries>
     </div>
   );
 }
+
+function Boost(){
+
+  // useState use like a function in react when upadte event hendler
+  const [count, setCount] = useState(0);
+  const increase = () =>{
+    const newCount=count+1
+    setCount(newCount)
+  }
+  return(
+    <div>
+      <h2>power : {count}</h2>
+      <button onClick={increase}>Boost</button>
+    </div>
+  )
+  }
+
+  function Countries(){
+    const [countries, setCountries] =useState([])
+    useEffect( () =>{
+
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    },[])
+    return(
+      <div>
+        <h1>{countries.length}</h1>
+         <div className='row  '>
+         {
+          countries.map(country => <Country name={country.name.common} area={country.area} image={country.flags.png}></Country> )
+         }
+         </div>
+        
+      </div>
+    )
+  }
+  function Country(props){
+    console.log(props)
+    return(
+      <div className='countries-container col-lg-3 g-2'>
+        <div>
+        <img style={{width: "250px"}} src={props.image}/>
+        <h3>Name : {props.name}</h3>
+        <h3>Area : {props.area}</h3>
+        </div>
+      </div>
+    )
+  }
 
 export default App;
